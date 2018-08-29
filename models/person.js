ec31d15c15b8c0c20ab929e29ccedd8702c81206
jusalanen <jussi.alanen@fonet.fi>
2018-08-29 19:28:58 +0300

    lisää format metodi mongoose.modeliin

--- models/person.js
+++ models/person.js
@@ -1,13 +1,24 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 // ethän laita salasanaa Githubiin!
-const url = 'mongodb://**********:puhluettO3@ds217092.mlab.com:17092/fullstack-phonebook'
+const url = 'mongodb://o3backend:**********@ds217092.mlab.com:17092/fullstack-phonebook'
 
 mongoose.connect(url, { useNewUrlParser: true }) //NewUrlParser to prevent DeprecationWarning
 
-const Person = mongoose.model('Person', {
+let personSchema = new Schema({
   name: String,
   number: String
 })
+personSchema.statics.format = function(person) {
+  return {
+    name: person.name,
+    number: person.number,
+    id: person._id
+  }
+}
+
+const Person = mongoose.model('Person', personSchema)
+
 
 module.exports = Person
