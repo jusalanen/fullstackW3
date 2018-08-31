a3dab38589b8827f22a2e443b8246b6898e53335
jusalanen <jussi.alanen@fonet.fi>
2018-08-31 17:03:30 +0300

    muuttaa backend url suhteelliseksi

--- o3frontend/personService.js
+++ o3frontend/personService.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://stark-scrubland-85118.herokuapp.com/api/persons/'
-
+const baseUrl = '/api/persons/'
 
 const getAll = () => {
   return axios.get(baseUrl)
