de15526b7661c7fcb66e6ba1f18e04091577638c
jusalanen <jussi.alanen@fonet.fi>
2018-09-06 20:12:57 +0300

    lisää routet api/persons/:id ja info

--- index.js
+++ index.js
@@ -5,135 +5,112 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
-
-let persons = []
-/*  {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Valtteri Vihavainen",
-      "number": "0400-4561145",
-      "id": 2
-    },
-    {
-      "name": "Laura Virtanen-Tukiainen",
-      "number": "044-789654",
-      "id": 3
-    },
-    {
-      "name": "Charlie Brown",
-      "number": "555-321456",
-      "id": 4
-    }
-]*/
-
 app.use(morgan('tiny'))
 app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('build'))
 
-const Info = () => {
-    const now = new Date()
-    const size = persons.length
-    const text = "Puhelinluettelossa " + size + " henkilön tiedot <br><br>" + now.toString()
-    return(       
-        text 
-    )
-}
-
-  app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+ 
+app.get('/api/persons', (req, res) => {
+  Person
+  .find({})
+  .then(persons => {
+    const people = persons.map(Person.format)
+    res.json(people)
   })
-  
-  app.get('/api/persons', (req, res) => {
-    Person
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+app.get('/info', (req, res) => {   
+  const now = new Date()
+  Person
     .find({})
-    .then(persons => {
-      const people = persons.map(Person.format)
-      res.json(people)
+    .then(result => {
+      const size = result.length
+      const text = "Puhelinluettelossa " + size + " henkilön tiedot <br><br>" + now.toString()
+      res.send(text)
+    }).catch(error => {
+      console.log(error)
+    })
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  Person
+    .findById(req.params.id)
+    .then(person => { 
+      res.json(Person.format(person))
+    }).catch(error => {
+      console.log(error)
+      res.status(400).json({ error: 'bad id' })
+    })    
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  Person
+    .findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    }).catch(error => {
+      console.log(error)
+      res.status(400).json({ error: 'bad id' })
+    })    
+})
+
+app.put('/api/persons/:id', (req, res) => {
+  const person = {
+    name: req.body.name,
+    number: req.body.number
+  }
+
+  Person
+    .findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(Person.format(updatedPerson))
     })
     .catch(error => {
       console.log(error)
+      res.status(400).json({ error: 'bad id' })
     })
-  })
+})
 
-  app.get('/info', (req, res) => {   
-    res.send(Info())
-  })
-
-  app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
+app.post('/api/persons', (req, res) => {
+  if (req.body.name === undefined || req.body.number === undefined) {
+    return res.status(400).json({error: 'content missing'})
+  }
+    
+  console.log(req.body)
   
-    if ( person ) {
-      res.json(person)
-    } else {
-      res.status(404).end()
-    }
-  })
-
-  app.delete('/api/persons/:id', (req, res) => {
-    Person
-      .findByIdAndRemove(req.params.id)
-      .then(result => {
-        res.status(204).end()
-      }).catch(error => {
-        console.log(error)
-        res.status(400).send({ error: 'bad id' })
-      })    
-  })
-
-  app.put('/api/persons/:id', (req, res) => {
-    const person = {
-      name: req.body.name,
-      number: req.body.number
-    }
-
-    Person
-      .findByIdAndUpdate(req.params.id, person, { new: true })
-      .then(updatedPerson => {
-        res.json(Person.format(updatedPerson))
-      })
-      .catch(error => {
-        console.log(error)
-        res.status(400).send({ error: 'bad id' })
-      })
-  })
-
-  app.post('/api/persons', (req, res) => {
-    if (req.body.name === undefined || req.body.number === undefined) {
-      return res.status(400).json({error: 'content missing'})
-    }
-    
-    console.log(req.body)
-    
-    if (req.body.name === '' || req.body.number === '') {
-      return res.status(400).json({error: 'name or number missing'})
-    } 
-    const p = persons.find(person => person.name === req.body.name)
+  if (req.body.name === '' || req.body.number === '') {
+    return res.status(400).json({error: 'name or number missing'})
+  }
+  Person
+    .find({})
+    .then(persons => {
+      const p = persons.find(person => person.name === req.body.name)
     if ( p ) {
       return res.status(400).json({error: 'name must be unique'})
     } else {
       const person = new Person({
-          name: req.body.name,
-          number: req.body.number
+        name: req.body.name,
+        number: req.body.number
       })
      
       person
         .save()
         .then(savedPerson => {
-            res.status(201).json(Person.format(savedPerson))
-            console.log(savedPerson)
+          res.status(201).json(Person.format(savedPerson))
+          console.log(savedPerson)
         })
         .catch(error => {
-            console.log(error)
+          console.log(error)
         })   
-      }
-    })
-  
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    }  
   })
+})
+  
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
