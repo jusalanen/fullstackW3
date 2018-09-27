6f654a2e5934c27048a66d893ccad62766b465b5
jusalanen <jussi.alanen@fonet.fi>
2018-09-27 19:51:12 +0300

    päivittää mongo.js

--- mongo.js
+++ mongo.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
-// ethän laita salasanaa Githubiin!
-const url = 'mongodb://o3backend:**********@ds217092.mlab.com:17092/fullstack-phonebook'
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true }) //NewUrlParser to prevent DeprecationWarning
 
@@ -11,30 +13,30 @@ const Person = mongoose.model('Person', {
 })
 
 if (process.argv.length === 2) {
-    console.log('')
-    console.log('puhelinluettelo:')
-    Person
-        .find({})
-        .then(result => {
-            result.forEach(person => {
-                console.log(person.name + ' ' + person.number)
-            })
-            mongoose.connection.close()
-        })
-} else if (process.argv.length === 4) {
-    const na = process.argv[2]
-    const nu = process.argv[3]
-    const person = new Person({
-        name: na,
-        number: nu
+  console.log('')
+  console.log('puhelinluettelo:')
+  Person
+    .find({})
+    .then(result => {
+      result.forEach(person => {
+        console.log(person.name + ' ' + person.number)
+      })
+      mongoose.connection.close()
     })
-    person
-        .save()
-        .then(response => {
-            console.log('lisätään henkilö ' +person.name+ ' numero ' +person.number+' luetteloon')
-            mongoose.connection.close()
-        })
+} else if (process.argv.length === 4) {
+  const na = process.argv[2]
+  const nu = process.argv[3]
+  const person = new Person({
+    name: na,
+    number: nu
+  })
+  person
+    .save()
+    .then(response => {
+      console.log('lisätään henkilö ' +person.name+ ' numero ' +person.number+' luetteloon')
+      mongoose.connection.close()
+    })
 } else {
-    console.log('tiedot annettu väärin')
-    mongoose.connection.close()
+  console.log('tiedot annettu väärin')
+  mongoose.connection.close()
 }
